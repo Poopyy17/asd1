c3260eded89f866cd28a3a75a1a8be281723e5d9
Jeyms <james.boac@gmail.com>
2024-08-27 15:27:08 +0800

    asd

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,7 @@ const Hero = () => {
   return (
     <div className="relative h-[450px] md:h-[600px] lg:h-[800px] w-full bg-transparent flex flex-col items-center justify-center text-center overflow-hidden">
       {/* Main content */}
-      <h1 className="relative font-dinnext font-black leading-tight animate-smooth-reveal lg:text-6xl md:text-4xl sm:text-3xl text-xl text-[#151515]">
+      <h1 className="relative font-dinnext font-black leading-tight animate-smooth-reveal lg:text-6xl md:text-4xl sm:text-3xl text-xl text-gray-800">
         Rotary Club of Las Piñas
       </h1>
       <p className="relative lg:text-2xl md:text-xl sm:text-lg text-base text-[#747474] tracking-widest font-sans italic font-normal mt-3">
